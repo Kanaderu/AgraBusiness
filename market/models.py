71154794a03067a373c64cf642bb69bef6a547f9
Unknown <fand.kanade@gmail.com>
2018-02-27 22:25:58 -0500

    Initial commit of AgraBusiness using Django
    First initial setup of the Django web application with a "market" application. Database and superuser need to be setup.
    
    To setup database:
    run 'python manage.py makemigrations'
    run 'python manage.py migrate'
    
    To setup superuser:
    run 'python manage.py createsuperuser'
    
    To run server:
    run 'python manage.py runserver'
    
    To flush database:
    run 'python manage.py flush'

---
+++ market/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User as dUser
+
+
+class CreditCard(models.Model):
+    name = models.CharField(max_length=256, help_text="Name on Credit Card")
+    number = models.IntegerField(help_text="Credit Card Number")  # normally a 12 digit integer
+    ccv = models.IntegerField(help_text="Credit Card CCV (Card Verification Value)")  # normally a 3 or 4 digit integer
+    exp = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class BankAccount(models.Model):
+    first_name = models.CharField(max_length=256, help_text="First Name Associated with Bank Account")
+    last_name = models.CharField(max_length=256, help_text="Last Name Associated with Bank Account")
+    routing_number = models.IntegerField(help_text="Bank Account Routing Number")  # normally a 9 digit integer
+    account_number = models.IntegerField(help_text="Bank Account Account Number")  # normally a 17 digit integer
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class PaymentMethod(models.Model):
+    billing_address_line1 = models.CharField(max_length=256, help_text="Billing Address Line 1")
+    billing_address_line2 = models.CharField(max_length=256, help_text="Billing Address Line 2")
+    billing_zip_code = models.IntegerField(help_text="Billing Address Zip Code")
+    billing_state = models.CharField(max_length=2, help_text="Billing Address State")
+    billing_city = models.CharField(max_length=256, help_text="Billing Address City")
+    billing_credit_card = models.ForeignKey('CreditCard', on_delete=models.SET_NULL, null=True)
+    billing_bank_account = models.ForeignKey('BankAccount', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.billing_address_line1
+
+
+class User(models.Model):
+    USER_TYPE = (
+        ('f', 'Farmer'),
+        ('c', 'Customer'),
+        ('m', 'Manager')
+    )
+
+    user = models.OneToOneField(dUser, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=1, choices=USER_TYPE, default='c', help_text="User Account Type")
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    payment_method = models.OneToOneField(PaymentMethod, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
