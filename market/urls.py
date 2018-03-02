582f54f86700eef4680c3713c67d93c9c9240223
Unknown <fand.kanade@gmail.com>
2018-03-01 20:33:09 -0500

    Fixed URL namespaces and added some quick links to the html pages

--- market/urls.py
+++ market/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls import url
 urlpatterns = [
     #url(r'^user/$', views.update_profile, {'template_name': 'user.html'}, name='user'),
     url(r'^user/$', views.edit_user, name='user'),
-    url(r'^register/$', views.register, name='user'),
+    url(r'^register/$', views.register, name='register'),
     #path('admin/', admin.site.urls),
 ]
