b9c394d81d7b0e518f691ac950bb7e181b9a8a0d
Unknown <fand.kanade@gmail.com>
2018-04-24 00:31:44 -0400

    Updated Order Details View

--- AgraBusiness/urls.py
+++ AgraBusiness/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     re_path(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     re_path(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
+            auth_views.password_reset_confirm, name='password_reset_confirm'),
     re_path(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     re_path(r'^favicon.ico$', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False), name="favicon"),
